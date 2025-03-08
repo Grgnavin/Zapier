52ad461d3086bc9d82073468962e187255bbaefc
Navin grg <grgnabin60@gmail.com>
2025-03-08 17:32:21 +0545

    first commit

---
+++ hooks/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.post("/hooks/catch/:userid/:zapid", (req, res) => {
+    const userId = req.params.userid;
+    const zapId = req.params.zapid;
+
+    //store new trigger in a db
+    
+    //push it into the queue(like rabbitmq or kafka)
+    
+
+
+})
